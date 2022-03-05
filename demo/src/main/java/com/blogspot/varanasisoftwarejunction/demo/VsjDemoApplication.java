9a3af625d10d96e6d8537b235b760d9d5bac9f1a
Champak's World <champaksworld@gmail.com>
2022-03-05 09:55:51 +0530

    Starting Spring Rest Controllers

--- demo/src/main/java/com/blogspot/varanasisoftwarejunction/demo/VsjDemoApplication.java
+++ demo/src/main/java/com/blogspot/varanasisoftwarejunction/demo/VsjDemoApplication.java
@@ -3,26 +3,42 @@ package com.blogspot.varanasisoftwarejunction.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
 public class VsjDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VsjDemoApplication.class, args);
-		System.out.println("hello");
-	}
-	@GetMapping("/test")
+    public static void main(String[] args) {
+        SpringApplication.run(VsjDemoApplication.class, args);
+        System.out.println("hello");
+    }
 
-	public String doTest(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s", name);
-	}
-	@GetMapping("/add")
+    @GetMapping("/test")
 
-	public String doAdd(@RequestParam(value = "a", defaultValue = "0") int a,@RequestParam(value = "b", defaultValue = "0") int b) {
-		return String.format("%s +  %s = %s", a,b,a+b);
-	}
+    public String doTest(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s", name);
+    }
 
+    @GetMapping("/add")
+
+    public String doAdd(@RequestParam(value = "a", defaultValue = "0") int a, @RequestParam(value = "b", defaultValue = "0") int b) {
+        return String.format("%s +  %s = %s", a, b, a + b);
+    }
+
+    @GetMapping(value = {"/doadd", "/doadd/{a}", "/doadd/{a}/{b}"})
+
+
+    public String getBook(@PathVariable Optional<Integer> a, @PathVariable Optional<Integer> b) {
+        int x = 0, y = 0;
+        if (a.isPresent())
+            x = a.get();
+        if (b.isPresent())
+            y = b.get();
+        return String.format("%s +  %s = %s", x, y, x + y);
+    }
 }
