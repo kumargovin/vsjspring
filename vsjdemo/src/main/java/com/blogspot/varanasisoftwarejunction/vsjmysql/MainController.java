39b65598746734b0bc074e1e65841a9b7c48c66d
Champak's World <champaksworld@gmail.com>
2022-04-07 10:34:37 +0530

    Changed the Port No

--- vsjdemo/src/main/java/com/blogspot/varanasisoftwarejunction/vsjmysql/MainController.java
+++ vsjdemo/src/main/java/com/blogspot/varanasisoftwarejunction/vsjmysql/MainController.java
@@ -3,10 +3,7 @@ package com.blogspot.varanasisoftwarejunction.vsjmysql;
 import javafx.scene.canvas.GraphicsContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller	// This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
@@ -30,7 +27,20 @@ private BookRepository bookRepository;
 		userRepository.save(n);
 		return "Saved";
 	}
-	@GetMapping(path="/addbook") // Map ONLY POST Requests
+	@PostMapping(path="/addbookobject")
+	public @ResponseBody String addNewBookObject (@RequestParam Book book) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+try {
+	bookRepository.save(book);
+	return "Saved";
+}
+catch (Exception ex)
+{
+	return ex.getMessage();
+}
+	}
+	@GetMapping(path="/addbook")
 	public @ResponseBody String addNewBook (@RequestParam String bookName
 			, @RequestParam String subject, @RequestParam Integer price) {
 		// @ResponseBody means the returned String is the response, not a view name
